3f59b9df221084f63018150f27bde4b8b9e39128
Shaun-da-Sheep <156547161+Shaun-da-Sheep@users.noreply.github.com>
2024-08-30 17:17:27 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -6,7 +6,7 @@ var config_data = `
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials", 
-      "code": "s",
+      "code": "s", 
       "type": "scouter",
       "size": 5,
       "maxSize": 5,
