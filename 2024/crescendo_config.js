695870ac857e7d265e825fb18a021e1a5d1f11c1
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-01-07 15:21:02 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -64,7 +64,7 @@ var config_data = `
       "shape": "circle 5 black red true"
     }
   ],
-  "auton": [
+  "auto": [
      { "name": "Leave Starting Zone",
       "code": "al",
       "type": "bool"
