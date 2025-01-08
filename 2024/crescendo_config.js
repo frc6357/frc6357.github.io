7a2aca2f7c40665dfdb63b849cbc9eeddba60000
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-01-08 15:06:51 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -58,7 +58,7 @@ var config_data = `
     { "name": "Auto Start Position",
       "code": "as",
       "type": "clickable_image",
-      "filename": "2024/real2025field.png",
+      "filename": "2024/realreal2025field.png",
       "clickRestriction": "one",
       "allowableResponses": "5 6 7 8 18 19 20 21 22 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 ",
       "shape": "circle 5 black red true"
