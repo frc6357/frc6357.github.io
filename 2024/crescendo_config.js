c2de17f7469098844677b048c81c685d964af75f
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-01-07 18:28:39 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -60,7 +60,7 @@ var config_data = `
       "type": "clickable_image",
       "filename": "2024/real2025field.png",
       "clickRestriction": "one",
-      "allowableResponses": "15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 48 49 50 52 55 80 81 83 85 90 91 92 95 97 100 101 105 106 107 108 109 110 111 112",
+      "allowableResponses": "15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55",
       "shape": "circle 5 black red true"
     }
   ],
