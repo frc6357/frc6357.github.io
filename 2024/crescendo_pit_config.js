a971fe161682ba469e30f0f23326f761c8c0ab9b
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-01-08 09:48:46 -0600

    Update crescendo_pit_config.js

--- 2024/crescendo_pit_config.js
+++ 2024/crescendo_pit_config.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "dataFormat": "tsv", 
-  "title": "Scouting PASS 2025",
+  "title": "Scouting PASS 2024",
   "page_title": "Pit Scouting",
   "pitConfig": "true",
   "prematch": [
