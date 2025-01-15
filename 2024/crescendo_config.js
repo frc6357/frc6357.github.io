d2729454be37ca242974c6db73d45c14e16db5ef
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-01-14 18:07:22 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -2,7 +2,7 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "Scouting PASS 2025",
-  "page_title": "6357 Reefscape 🪸",
+  "page_title": "63573 Reefscape 🪸",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
