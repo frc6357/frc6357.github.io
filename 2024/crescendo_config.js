7b2b591051ef7763735e09043181ebc577e6c712
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-03-07 09:26:27 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025txbel",
+      "defaultValue": "2025TXBEL",
       "required": "true"
     },
     { "name": "Match Level",
