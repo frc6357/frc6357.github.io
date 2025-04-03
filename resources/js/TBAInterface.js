fcbea39fa04e001e8b3dd92c9a3e8e9acd5a0210
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-04-03 11:19:26 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var eventCode = "2025TXCLE"
+var eventCode = "2025TXCMP2"
 var authKey = "ohnpjLNvzysqKFuCX6aWLtPNlgrc1gYom9Wj1oBiN6w4NXdFmNKI7X3AoNxHyVMh";
 /**
  * Get list of teams in event
