ec7c1fda75176f60106d81b5d1f8ca949705db1d
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-04-17 08:17:49 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025JCMP",
+      "defaultValue": "2025JOHNSON",
       "required": "true"
     },
     { "name": "Match Level",
