6be083eb7d006148b913b20bb49c0f86a50c3d1d
Shaun <156547161+Shaun-da-Sheep@users.noreply.github.com>
2025-04-17 07:43:55 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "HOPPER",
+      "defaultValue": "2025HOPPER",
       "required": "true"
     },
     { "name": "Match Level",
